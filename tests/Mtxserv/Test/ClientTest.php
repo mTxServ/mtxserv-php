f6a302a70f51f5358825fe27488b4c81c006051d
Seb <trakiss@gmail.com>
2014-12-23 08:55:04 +0100

    remove useless API version in user agent

--- tests/Mtxserv/Test/ClientTest.php
+++ tests/Mtxserv/Test/ClientTest.php
@@ -21,4 +21,17 @@ class ClientTest extends GuzzleTestCase
         
         $this->assertEquals('https://www.mtxserv.fr/api/v1337/', $client->getBaseUrl());
     }
+    
+    public function testFactoryInitializesUserAgent()
+    {
+        $client = Client::factory(array(
+            'client_id'     => 'CLIENT_ID',
+            'client_secret' => 'CLIENT_SECRET',
+            'api_key'       => 'API_KEY',
+            'version'       => 'v1337'
+        ));
+        $request = $client->get();
+        $this->assertEquals('mTxServ SDK PHP', (string) $request->getHeader('User-Agent'));
+    }
+
 }
